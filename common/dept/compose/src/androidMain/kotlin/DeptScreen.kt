77d8667408d81576b70557628a2eaddfdbc55ad1
Nifont <nifontbus@yandex.ru>
2023-07-01 21:27:37 +0300

    +dept lifeCycle

--- common/dept/compose/src/androidMain/kotlin/DeptScreen.kt
+++ common/dept/compose/src/androidMain/kotlin/DeptScreen.kt
@@ -1,4 +1,10 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.adeo.kviewmodel.compose.observeAsState
 import com.adeo.kviewmodel.odyssey.StoredViewModel
 import ru.alexgladkov.odyssey.compose.local.LocalRootController
@@ -6,6 +12,16 @@ import ru.alexgladkov.odyssey.compose.local.LocalRootController
 @Composable
 fun DeptScreen() {
 	val rootController = LocalRootController.current
+
+	OnLifecycleEvent { owner, event ->
+		println("DeptScreen $event, $owner")
+		when (event) {
+			Lifecycle.Event.ON_RESUME -> {
+
+			}
+			else -> {  }
+		}
+	}
 
 	StoredViewModel({ DeptViewModel() }) { viewModel ->
 		val viewState = viewModel.viewStates().observeAsState()
@@ -19,6 +35,24 @@ fun DeptScreen() {
 		when (viewAction.value) {
 			null -> {}
 			else -> {}
+		}
+	}
+}
+
+@Composable
+fun OnLifecycleEvent(onEvent: (owner: LifecycleOwner, event: Lifecycle.Event) -> Unit) {
+	val eventHandler = rememberUpdatedState(onEvent)
+	val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+
+	DisposableEffect(lifecycleOwner.value) {
+		val lifecycle = lifecycleOwner.value.lifecycle
+		val observer = LifecycleEventObserver { owner, event ->
+			eventHandler.value(owner, event)
+		}
+
+		lifecycle.addObserver(observer)
+		onDispose {
+			lifecycle.removeObserver(observer)
 		}
 	}
 }
